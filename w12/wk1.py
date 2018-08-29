63e1d34f90e125aa0a280b00c8d454aa5db7e708
shants <mailtoshants@gmail.com>
2018-08-29 10:42:02 -0400

    remove class O

--- w12/wk1.py
+++ w12/wk1.py
@@ -12,28 +12,6 @@ def double(x):
 def sumAndproduct(x=0,y=0):
   return (x+y,x*y)
 
-"""
-class O:
-  y=n=0
-  @staticmethod
-  def report():
-    print("\n# pass= %s fail= %s %%pass = %s%%"  % (
-          O.y,O.n, int(round(O.y*100/(O.y+O.n+0.001)))))
-  @staticmethod
-  def k(f):
-    try:
-      print("\n-----| %s |-----------------------" % f.__name__)
-      if f.__doc__:
-        print("# "+ re.sub(r'\n[ \t]*',"\n# ",f.__doc__))
-      f()
-      print("# pass")
-      O.y += 1
-    except:
-      O.n += 1
-      print(traceback.format_exc()) 
-    return f
-"""
-
 @O.k
 def testingSucess_WhiteSpace():
   ans = 1 in [1,2,3,4,5]
