9a70492560cdaff1a478ae0f6bcb7a2cdcd1dd27
shants <mailtoshants@gmail.com>
2018-09-09 15:56:55 -0400

    finally syms work

--- w3/Sym.py
+++ w3/Sym.py
@@ -2,43 +2,47 @@ import math
 
 class Sym:
     def __init__(self):
-        self.counts=[]
+        self.counts={}
         self.mode = None
         self.most = 0
         self.n = 0
         self._ent = None
 
-    def syms(self, f):
-        f = f if f else lambda x: x
-        for x in self.counts:
-            self.symInc(f(x))
-        return
 
-    def syncInc(self, x):
-        if x == ?:
+    def syms(self,t, f = None):
+        if f == None:
+            f =  lambda x: x
+        s = Sym()
+        for x in t:
+            s.symInc(f(x))
+        return s
+
+    def symInc(self, x):
+        if x == '?':
             return x
         self._ent = None
-        t.n = t.n+1
-        old = self.counts[x] # no check for x ?
-        new = old and old+1 or 1
-        t.counts[x] = new
-        if new > t.most:
-            t.most, t,mode = new, x
+        self.n = self.n+1
+        old = self.counts.get(x,0) 
+        new = old and old+1 or 1 # what is this ??
+        self.counts[x] = new
+        if new > self.most:
+            self.most, self.mode = new, x
 
         return x
 
     def symDec(self, x):
         self._ent = None
-        t.n = t.n -1
-        #i.counts[x] = i.counts - 1
+        self.n = self.n -1
+        self.counts[x] = self.counts[x] - 1
         return x
 
     def symEnt(self):
-        if not self._ent:
+        if self._ent == None:
             self._ent = 0
-        for x,n in enumerate(self.counts):
-            p = n / self.n
-            self._ent = self._ent - p*math.log(p,2)
+            p = 0
+            for k,v in self.counts.items():
+                p = v / self.n
+                self._ent = self._ent - p*math.log(p,2)
 
         return self._ent
 
--- w3/tests/tests.py
+++ w3/tests/tests.py
@@ -5,6 +5,8 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 from Sample import Sample
+from Sym import Sym
+from Num import Num
 from testingModule import O
 import random
 
@@ -23,8 +25,8 @@ def testing_Sample():
     #-- 10,000 store the same number in all samples
     for i in range(1,10000):
         y= random.random()
-    for t in s:
-        t.sampleInc(y)
+        for t in s:
+            t.sampleInc(y)
 
     #-- check if any of them are +/- 0.2 of 0.5
     for t in s: 
@@ -35,12 +37,27 @@ def testing_Sample():
 
 @O.k
 def testing_Sym():
-    assert 1 == 1
+    s1 = Sym()
 
+    s1 = s1.syms([ 'y','y','y','y','y','y','y','y','y', 'n','n','n','n','n'])
+    print(s1.symEnt())
+    assert  round(s1.symEnt(),4) ==  0.9403
+    #assert 1 == 1
+
+
+"""
 @O.k
 def testing_Num():
-    assert 1 == 1
+    n1 = Num()
+    n = n1.nums([ 4,10,15,38,54,57,62,83,100,100,174,190,215,225,
+       233,250,260,270,299,300,306,333,350,375,443,475,
+       525,583,780,1000])
 
+    print(n.mu, n.sd)
+    assert(n.mu == 270.3 )
+    assert(n.sd == 231.946)
+    
+"""
 
 if __name__== "__main__":
   O.report()
