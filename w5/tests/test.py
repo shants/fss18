360b84b045ca0983d4fe6defc11a0344fbaed90a
shants <mailtoshants@gmail.com>
2018-09-27 23:44:18 -0400

    import testingModule

--- w5/tests/test.py
+++ w5/tests/test.py
@@ -1,31 +1,7 @@
 #from .testingModule import O
 from dom import Dom
 from rows import Data
-import re
-import traceback
-class O:
-  y=n=0
-  @staticmethod
-  def report():
-    print("\n# pass= %s fail= %s %%pass = %s%%"  % (
-          O.y,O.n, int(round(O.y*100/(O.y+O.n+0.001)))))
-  @staticmethod
-  def k(f):
-    try:
-      print("\n-----| %s |-----------------------" % f.__name__)
-      if f.__doc__:
-        print("# "+ re.sub(r'\n[ \t]*',"\n# ",f.__doc__))
-      f()
-      print("# pass")
-      O.y += 1
-    except:
-      O.n += 1
-      print(traceback.format_exc())
-    return f
-
-
-
-
+from testingModule import O
 
 @O.k
 def test_weather():
